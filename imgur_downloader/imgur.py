9eea51c3a360e0f9f15951d9a7fb99c04346f3ed
Widnyana <widnyana.p@gmail.com>
2013-11-20 05:05:02 +0700

    a little housekeeping

--- imgur_downloader/imgur.py
+++ imgur_downloader/imgur.py
@@ -2,21 +2,35 @@
 # imgur downloader.
 
 
-import re, urllib, urllib2
+import re, urllib, urllib2, os
+from time import gmtime, strftime
 
-url = raw_input("Imgur url: ").strip()
-print "Extracting %s" % url
+def main():
+    chan = raw_input("Imgur Channel: ").strip()
+    folder = ("%s/%s") % (chan, strftime("%Y-%m-%d", gmtime()))
+    url = "http://imgur.com/r/%s" % chan
 
-html_page       = urllib2.urlopen(url)
-html_source     = html_page.readlines()
-html_page.close()
+    try:
+        os.makedirs(folder)
+    except Exception, e:
+        print e
+        pass
 
-for line in html_source:
-    p = re.compile(r'\/\/i.imgur.com\/[0-9a-zA-Z]+')
-    iterator = p.finditer(line)
+    print "Extracting %s" % url
 
-    for match in iterator:
-        target = "http:%s.jpg" %(match.group()[:-1])
-        name = "%s" %(match.group()[14:-1])
-        print "Downloading %s ..." %(target)
-        urllib.urlretrieve(target, name)
+    html_page       = urllib2.urlopen(url)
+    html_source     = html_page.readlines()
+    html_page.close()
+    for line in html_source:
+        p = re.compile(r'\/\/i.imgur.com\/[0-9a-zA-Z]+')
+        iterator = p.finditer(line)
+
+        for match in iterator:
+            target = "http:%s.jpg" %(match.group()[:-1])
+            name = "%s" %(match.group()[14:-1])
+            print "Downloading %s..." %(target)
+            saveto = "%s/%s" % (folder, name)
+            urllib.urlretrieve(target, saveto)
+
+if __name__ == '__main__':
+    main()
