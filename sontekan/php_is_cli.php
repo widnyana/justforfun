0ffb371284a836c003aedc3a375afdbe45c5399a
widnyana <widnyana.p@gmail.com>
2016-04-05 13:28:39 +0700

    is_cli ?

---
+++ sontekan/php_is_cli.php
@@ -0,0 +1,27 @@
+<?php
+
+function is_cli()
+{
+    if ( defined('STDIN') )
+    {
+        return true;
+    }
+
+    if ( php_sapi_name() === 'cli' )
+    {
+        return true;
+    }
+
+    if ( array_key_exists('SHELL', $_ENV) ) {
+        return true;
+    }
+
+    if ( empty($_SERVER['REMOTE_ADDR']) and !isset($_SERVER['HTTP_USER_AGENT']) and count($_SERVER['argv']) > 0) 
+    {
+        return true;
+    } 
+
+    return false;
+}
+
+echo "script running from: " . (is_cli() ? "cli" : "http");
